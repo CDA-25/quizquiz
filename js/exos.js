29402bbcfaa2a4323283e24b1e0771cc0325c837
Quentyn404 <quentyn.petitjean@hotmail.com>
2025-05-09 10:49:11 +0200

    exo sans bonus

--- js/exos.js
+++ js/exos.js
@@ -7,11 +7,28 @@
 
 /* Exo 1 */
 // Ton code ici...
+let question1 = `Quelle mer borde la ville de Sébastopol ?`;
 
+let solution1 = "la mer Noire";
 
 /* Exo 2 */
 // Ton code ici...
-
+let reponse1 = prompt(question1);
+if (reponse1 === solution1) {
+  alert("Gagné !");
+} else {
+  alert("perdu");
+}
 
 /* Exo 3 */
 // Ton code ici...
+let question2 = `Quel est l'âge du capitaine ?`;
+
+let solution2 = "63";
+
+let reponse2 = prompt(question2);
+if (reponse2 === solution2) {
+  alert("Gagné !");
+} else {
+  alert("perdu");
+}
