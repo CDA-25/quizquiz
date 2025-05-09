7a6b2e96ced68a8f93aa169625ff240c3b512d7e
Psykup25 <romg25@gmail.com>
2025-05-09 10:57:44 +0200

    AJout de la fonction Bonus

--- js/exos.js
+++ js/exos.js
@@ -6,12 +6,40 @@
  */
 
 /* Exo 1 */
-// Ton code ici...
-
+let question1 = "Quelle mer borde la ville de Sébastopol ?"
+let solution1 = "la mer Noire"
 
 /* Exo 2 */
-// Ton code ici...
 
+let reponse1 = prompt(question1);
+if (reponse1 === solution1) {
+    alert("Gagné !");
+} else {  
+    alert("Perdu ...");
+}
 
 /* Exo 3 */
-// Ton code ici...
+let question2 = "Quel est l'âge du capitaine ?"
+let solution2 = 63
+
+let reponse2 = parseInt(prompt(question2))
+if (reponse2 === solution2) {
+    alert("Gagné !");
+}
+else {
+    alert("Perdu ...");
+}
+
+/* Bonus */
+
+let score = 0;
+
+if (reponse1 === solution1) {
+    score++;
+}
+
+if (reponse2 === solution2) {
+    score++;
+}
+
+alert("Vous avez " + score + " réponse(s) correcte(s).");
