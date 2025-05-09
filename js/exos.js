6add03432eea7f3a97ff925d2a448249c4a40a0e
Alice <alice.cordier39210@gmail.com>
2025-05-09 10:35:05 +0200

    Exo 2 : création du prompt et stockage de la réponse utilisateur

--- js/exos.js
+++ js/exos.js
@@ -14,7 +14,15 @@ let solution1 = "La mer Noire";
 
 /* Exo 2 */
 // Ton code ici...
+// on stock la reponse dans la variable reponse1 (donc var = le prompt(message))
+let reponse1 = prompt(question1);
 
+if (reponse1 === solution1) {
+    alert("Gagné !")
+
+} else {
+    alert("Perdu...")
+};
 
 /* Exo 3 */
 // Ton code ici...
