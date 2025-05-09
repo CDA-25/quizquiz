496cd676f6015ce6ca12903c057d1773dc0ac231
NicolasBondenet <36343149+Vaquicraft@users.noreply.github.com>
2025-05-09 11:14:10 +0200

    (quiz)^2 (ouais super nom de commit je sais)

--- js/exos.js
+++ js/exos.js
@@ -1,5 +1,3 @@
-
-
 /**
  *  Challenge Quizz
  *
@@ -8,12 +6,55 @@
  */
 
 /* Exo 1 */
-// Ton code ici...
-
+const question1 = String("Quelle mer borde la ville de Sébastopol ?")
+const solution1 = String("la mer Noire")
+let fautes = 0 //compteur de fautes
 
 /* Exo 2 */
-// Ton code ici...
+
+function questionReponse(question, reponse)
+{
+    // alert(question)  Je trouve ça plus intuitif de passer uniquement par le prompt() et passer la variable question dedans plutôt que via un alert()
+    output = prompt(question)
+
+    if (!output)
+    {
+        alert("Oh, tu dois écrire au moins quelque chose tu t'es cru où là ?")
+        fautes++
+        return;
+    }
+    if (output == reponse)
+    {
+        alert("Gagné ! =D")
+        return;
+    }
+    else
+    {
+        alert("Perdu... =(")
+        fautes++
+        return;
+    }
+}
+
+questionReponse(question1, solution1) //Appelle la fonction avec comme arguments les variables définies dans Exo 1
+
 
 
 /* Exo 3 */
-// Ton code ici...
+const question2 = String("Quel est l'âge du capitaine ?")
+const solution2 = Number(63)
+
+questionReponse(question2, solution2) //Appelle la fonction pour l'exo 2
+
+if (fautes == 0)
+{
+    alert("Vous avez terminé le quizz sans faire une seule faute, quel exploit !") // Ca flex severe ici
+}
+
+else
+{
+    alert("Vous avez terminé le quizz ! Vous avez fait " + fautes + " fautes") // Ca flex un peu moins
+
+}
+
+
