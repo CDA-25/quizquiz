c5c57cbc009bb600a4ba74ef7263a15c5b7400f2
Alexis <desousafreitasalexis@gmail.com>
2025-05-09 10:36:52 +0200

    Fini

--- js/exos.js
+++ js/exos.js
@@ -6,13 +6,29 @@
  */
 
 /* Exo 1 */
+let score = 0;
 let question1 = "Quelle mer borde la ville de Sébastopol ?";
 let solution1 = "la mer Noire";
 
-
 /* Exo 2 */
-// Ton code ici...
-
+let reponse1 = prompt(question1);
+if (reponse1 == solution1) {
+    score++;
+    alert("Gagné !"); 
+} else {
+    alert("Perdu ...");
+}
 
 /* Exo 3 */
-// Ton code ici...
+let question2 = "Quel est l'âge du capitaine ?";
+let solution2 = 63;
+let reponse2 = prompt(question2);
+if (reponse2 == solution2) {
+    score++;
+    alert("Gagné !");
+}
+else {
+    alert("Perdu ...");
+}
+
+alert("Votre score est de " + score + " / 2");
