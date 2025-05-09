f2f5f2db2c18cf5848bd5bbe71b507ae57e85867
O N <smiile.smilex@gmail.com>
2025-05-09 11:19:25 +0200

    Quizquiz exos 1 à 3

--- js/exos.js
+++ js/exos.js
@@ -7,11 +7,39 @@
 
 /* Exo 1 */
 // Ton code ici...
+let question1 = "Quelle mer borde la ville de Sébastopol ?"
+let solution1 = "La mer noire"
 
 
 /* Exo 2 */
 // Ton code ici...
+let reponse1 = prompt(question1)
+let nbBonneResponse=0;
+
+
+if (reponse1==solution1)
+    {
+        alert("Gagné !");
+        nbBonneResponse+=1;
+    }
+    else { alert("Perdu...")};
 
 
 /* Exo 3 */
 // Ton code ici...
+
+let question2 = "Quel est l'âge du capitaine ?"
+let solution2 = 63
+
+let reponse2 = parseInt(prompt(question2))
+
+if (reponse2==solution2)
+    {
+        alert("Gagné !");
+        nbBonneResponse+=1;
+    }
+    else alert("Perdu...");
+
+
+
+alert("Vous avez "+ nbBonneResponse+" bonnes réponse(s).")
