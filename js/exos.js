54ef2c7d55a70eaabb43ffe7e637430dd0ac81b2
Tarik25num <titi.rifpower@hotmail.fr>
2025-05-09 10:54:01 +0200

    Challenge finis supplément BONUS en prime et tuto pour Jérome avec le parseInt :wink-wink:

--- js/exos.js
+++ js/exos.js
@@ -7,11 +7,31 @@
 
 /* Exo 1 */
 // Ton code ici...
-
+let question1 = "Quelle mer borde la ville de Sébastopol ?";
+let solution1 = "La mer Noire";
+let score = 0 
 
 /* Exo 2 */
 // Ton code ici...
-
+    let reponse1 = prompt("Quelle mer borde la ville de Sébastopol ?");
+    if (reponse1 === solution1) {
+        score++
+        alert("Gagné ! T'es vraiment le boss du matosse !");
+    } else {
+        alert("Perdu.. If I speak, i'm in very big trouble.. I prefer not to speak.");
+    }
 
 /* Exo 3 */
 // Ton code ici...
+let question2 = "Quel est l'âge du capitaine ?"
+let solution2 = 63
+
+let reponse2 = parseInt(prompt("Quel est l'âge du capitaine ?"));
+if (reponse2 === solution2) {
+    score++
+    alert("C'est bien ça.");
+} else {
+    alert("Faux !!! au bûcher racaille !");
+}
+
+alert("Votre score est de : " + score + "/2")
